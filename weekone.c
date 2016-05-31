fded2d787a7bae6f835e3bb94ef67b9c8788f46d
ssww0819 <shaowen0819@yahoo.co.jp>
2016-05-31 16:06:51 +0900

    160531 commit1

---
+++ weekone.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    printf("File open failed\n");
+    return 1;
+  }
+
+  int c;
+  int ct;
+  while (( c = getc(fp)) != '\n') {
+    ct++;
+  }
+
+  fseek(fp, 0L, SEEK_SET);
+
+  
