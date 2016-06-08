b433dde0f1c1832c754eb2afd311e0fcbd1ee261
ssww0819 <shaowen0819@yahoo.co.jp>
2016-06-09 00:25:00 +0900

    in version1, it is difficult to make matrix with different sizes, so here is version2: initialize the matrix in the code

---
+++ weektwo_2.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+double get_time()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+double c(int i, int j, int m, double** A, double **B) {
+  double sum = 0;
+  int k;
+  for (k = 0;k < m;k++) {
+    sum += A[i][k] * B[k][j];
+  }
+  return sum;
+}
+
+void multiply (double **sol, double **A, double **B, int size) {
+  int i, j;
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      sol[i][j] = c(i, j, size, A, B);
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+  int size = atoi(argv[1]);
+  int a;
+  int i,j;
+  double **matrix1;
+  matrix1 = (double**)malloc(sizeof(double*) * size);
+  for (a = 0; a < size; a++) {
+    *(matrix1 + a) = (double*)malloc(sizeof(double) * size);
+  }
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      matrix1[i][j] = i * size + j;    
+    }
+  }
+  double **matrix2;
+  matrix2 = (double**)malloc(sizeof(double*) * size);
+  for (a = 0; a < size; a++) {
+    *(matrix2 + a) = (double*)malloc(sizeof(double) * size);
+  }
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      matrix2[i][j] = j * size + i;    
+    }
+  }
+  double **matrix3;
+  matrix3 = (double**) malloc(sizeof(double*) * size);
+  for (a = 0;a < size;a++) {
+    *(matrix3 + a) = (double*)malloc(sizeof(double) * size);
+  }
+  
+  double begin = get_time();
+
+  multiply(matrix3, matrix1, matrix2, size);
+  
+  double end = get_time();
+  
+  printf("time: %.6lf sec\n", end - begin);
+  // Print C for debugging. Comment out the print before measuring the execution time.
+  double sum = 0;
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      sum += matrix3[i][j];
+      // printf("c[%d][%d]=%lf\n", i, j, c[i * n + j]);
+    }
+  }
+  // Print out the sum of all values in C.
+  // This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+  printf("sum: %.6lf\n", sum);
+  
+  for (a = 0; a < size; a++) {
+    free (*(matrix1 + a));
+  }
+  free(matrix1);
+  free(matrix2);
+  free(matrix3);
+
+  return 0;
+}
